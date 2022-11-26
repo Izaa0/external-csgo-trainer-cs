651a617efb11b254accefcaa500e446b079bd60e
Izaa <108992411+Izaa0@users.noreply.github.com>
2022-11-26 16:28:19 +0100

    Add files via upload

---
+++ Menu.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace xp
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbIfov = new System.Windows.Forms.CheckBox();
+            this.cbFov = new System.Windows.Forms.CheckBox();
+            this.SlideriFov = new Guna.UI2.WinForms.Guna2TrackBar();
+            this.SliderFov = new Guna.UI2.WinForms.Guna2TrackBar();
+            this.cbAntiFlash = new System.Windows.Forms.CheckBox();
+            this.cbThirdPerson = new System.Windows.Forms.CheckBox();
+            this.cbBhop = new System.Windows.Forms.CheckBox();
+            this.cbTriggerbot = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cbIfov);
+            this.panel1.Controls.Add(this.cbFov);
+            this.panel1.Controls.Add(this.SlideriFov);
+            this.panel1.Controls.Add(this.SliderFov);
+            this.panel1.Controls.Add(this.cbAntiFlash);
+            this.panel1.Controls.Add(this.cbThirdPerson);
+            this.panel1.Controls.Add(this.cbBhop);
+            this.panel1.Controls.Add(this.cbTriggerbot);
+            this.panel1.Location = new System.Drawing.Point(6, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 321);
+            this.panel1.TabIndex = 0;
+            // 
+            // cbIfov
+            // 
+            this.cbIfov.AutoSize = true;
+            this.cbIfov.Location = new System.Drawing.Point(32, 202);
+            this.cbIfov.Name = "cbIfov";
+            this.cbIfov.Size = new System.Drawing.Size(45, 19);
+            this.cbIfov.TabIndex = 7;
+            this.cbIfov.Text = "Fov";
+            this.cbIfov.UseVisualStyleBackColor = true;
+            // 
+            // cbFov
+            // 
+            this.cbFov.AutoSize = true;
+            this.cbFov.Location = new System.Drawing.Point(32, 145);
+            this.cbFov.Name = "cbFov";
+            this.cbFov.Size = new System.Drawing.Size(48, 19);
+            this.cbFov.TabIndex = 6;
+            this.cbFov.Text = "iFov";
+            this.cbFov.UseVisualStyleBackColor = true;
+            // 
+            // SlideriFov
+            // 
+            this.SlideriFov.Location = new System.Drawing.Point(32, 227);
+            this.SlideriFov.Maximum = 200;
+            this.SlideriFov.Name = "SlideriFov";
+            this.SlideriFov.Size = new System.Drawing.Size(300, 23);
+            this.SlideriFov.TabIndex = 5;
+            this.SlideriFov.ThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
+            this.SlideriFov.Value = 90;
+            // 
+            // SliderFov
+            // 
+            this.SliderFov.Location = new System.Drawing.Point(32, 170);
+            this.SliderFov.Maximum = 200;
+            this.SliderFov.Name = "SliderFov";
+            this.SliderFov.Size = new System.Drawing.Size(300, 23);
+            this.SliderFov.TabIndex = 4;
+            this.SliderFov.ThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
+            this.SliderFov.Value = 90;
+            // 
+            // cbAntiFlash
+            // 
+            this.cbAntiFlash.AutoSize = true;
+            this.cbAntiFlash.Location = new System.Drawing.Point(32, 120);
+            this.cbAntiFlash.Name = "cbAntiFlash";
+            this.cbAntiFlash.Size = new System.Drawing.Size(75, 19);
+            this.cbAntiFlash.TabIndex = 3;
+            this.cbAntiFlash.Text = "AntiFlash";
+            this.cbAntiFlash.UseVisualStyleBackColor = true;
+            // 
+            // cbThirdPerson
+            // 
+            this.cbThirdPerson.AutoSize = true;
+            this.cbThirdPerson.Location = new System.Drawing.Point(32, 95);
+            this.cbThirdPerson.Name = "cbThirdPerson";
+            this.cbThirdPerson.Size = new System.Drawing.Size(89, 19);
+            this.cbThirdPerson.TabIndex = 2;
+            this.cbThirdPerson.Text = "ThirdPerson";
+            this.cbThirdPerson.UseVisualStyleBackColor = true;
+            // 
+            // cbBhop
+            // 
+            this.cbBhop.AutoSize = true;
+            this.cbBhop.Location = new System.Drawing.Point(32, 70);
+            this.cbBhop.Name = "cbBhop";
+            this.cbBhop.Size = new System.Drawing.Size(54, 19);
+            this.cbBhop.TabIndex = 1;
+            this.cbBhop.Text = "Bhop";
+            this.cbBhop.UseVisualStyleBackColor = true;
+            // 
+            // cbTriggerbot
+            // 
+            this.cbTriggerbot.AutoSize = true;
+            this.cbTriggerbot.Location = new System.Drawing.Point(32, 45);
+            this.cbTriggerbot.Name = "cbTriggerbot";
+            this.cbTriggerbot.Size = new System.Drawing.Size(80, 19);
+            this.cbTriggerbot.TabIndex = 0;
+            this.cbTriggerbot.Text = "Triggerbot";
+            this.cbTriggerbot.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "xphax.cc";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::xp.Properties.Resources.pngwing_com;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 361);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.Opacity = 0.85D;
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private CheckBox cbTriggerbot;
+        private CheckBox cbBhop;
+        private CheckBox cbThirdPerson;
+        private CheckBox cbAntiFlash;
+        private CheckBox cbIfov;
+        private CheckBox cbFov;
+        private Guna.UI2.WinForms.Guna2TrackBar SlideriFov;
+        private Guna.UI2.WinForms.Guna2TrackBar SliderFov;
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSGO;
+
+namespace xp
+{
+    public partial class Menu : Form
+    {
+        public const string WINDOW_NAME = "Counter-Strike: Global Offensive - Direct3D 9";
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        public static IntPtr handle = FindWindow(null, WINDOW_NAME);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT IpRect);
+
+        public static RECT rect;
+
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        csgo cs = new csgo();
+        int iFov, Fov;
+        
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            Thread main = new Thread(Main) { IsBackground = true };
+            main.Start();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            this.BackColor = Color.Wheat;
+            this.TransparencyKey = Color.Wheat;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+
+            
+
+
+            GetWindowRect(handle, out rect);
+            this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
+
+
+
+            this.Left = rect.left;
+            this.Top = rect.top;
+
+
+        }
+
+       
+
+        void Main()
+        {
+            cs.GetInformation();
+
+            while (true)
+            {
+                
+                if (cbTriggerbot.Checked)
+                {
+                    if(GetAsyncKeyState(Keys.ShiftKey) < 0)
+                    {
+                        cs.Triggerbot();
+                    }
+                    
+                }
+
+                if (GetAsyncKeyState(Keys.F12) < 0)
+                {
+                    this.Show();
+                }
+
+                if (GetAsyncKeyState(Keys.F11) < 0)
+                {
+                    this.Hide();
+                }
+
+                if(cbFov.Checked)
+                {
+                    Fov = SliderFov.Value;
+                    cs.FovChanger(Fov);
+                }
+
+                if(cbIfov.Checked)
+                {
+                    iFov = SlideriFov.Value;
+                    cs.iFovChanger(iFov);
+                }
+
+                if (cbThirdPerson.Checked)
+                {
+                    cs.EnableThirdPerson();
+                }
+                else
+                {
+                    cs.DisableThirdPerson();
+                }
+
+                if (cbBhop.Checked)
+                {
+                    if(GetAsyncKeyState(Keys.Space) < 0)
+                    {
+                        cs.Bhop();
+                    }
+                   
+                }
+
+                if (cbAntiFlash.Checked)
+                {
+                    cs.AntiFlash();
+                }
+            }
+            
+        }
+    }
+}
